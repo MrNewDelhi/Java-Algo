67eab3fc3ca473dbf9d9d67f6cda0bc5d8a399d5
anmol <anmolsoin1@gmail.com>
2022-01-07 15:51:26 +0530

    break

--- src/Lec_10/Sorted_Row_And_Column.java
+++ src/Lec_10/Sorted_Row_And_Column.java
@@ -2,26 +2,24 @@ package Lec_10;
 
 public class Sorted_Row_And_Column {
     public static void main(String[] args) {
-        int arr[][] = { {10,20,30,40},
-                        {15,25,35,45},
-                        {27,28,37,48},
-                        {29,33,39,50} };
+        int arr[][] = {{10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {27, 28, 37, 48},
+                {29, 33, 39, 50}};
     }
-    public static void Search(int[][] arr, int key){
-        int row =
-    }
-    public static int BinarySearch(int[][] arr, int start, int end, int row, int column, int key){
-        int ans = 0;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            if(arr[row][mid] == key){
-                ans = mid;
+
+    public static void Search(int[][] arr, int key) {
+        int row = 0;
+        int column = arr.length - 1;
+        while (column >= 0 && row < arr.length) {
+            if (arr[row][column] == key) {
+                System.out.println("Key found : " + row + "," + column);
+                break;
+            } else if (arr[row][column] > key) {
+                column--;
+            } else {
+                row++;
             }
-            else if(arr[row][mid] > key){
-                end = mid - 1;
-            }
-            else if(arr[row][mid] < key)
         }
-        return ans;
     }
 }
