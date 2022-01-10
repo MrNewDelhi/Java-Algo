492549bdde02684b0ef7d89fc8ce824b47b44f18
anmol <anmolsoin1@gmail.com>
2022-01-10 07:52:34 +0530

    Some more questions

--- src/Assignment1/Playing_with_Good_String.java
+++ src/Assignment1/Playing_with_Good_String.java
@@ -6,6 +6,35 @@ public class Playing_with_Good_String {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = sc.next();
+        int max = 0;
 
+        for (int start = 0; start < word.length(); start++) {
+            int length = 0;
+            if (isVowel(word.charAt(start))) {
+                length++;
+                for (int end = start + 1; end < word.length(); end++) {
+                    if (isVowel(word.charAt(end))) {
+                        length++;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+            if(length > max){
+                max = length;
+            }
+        }
+        System.out.println(max);
+    }
+
+    public static boolean isVowel(char character) {
+        String vowels = "aeiou";
+        for (int index = 0; index < vowels.length(); index++) {
+            if (character == vowels.charAt(index)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
