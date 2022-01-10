66af3a6b004037bbdfc4955122f4fc7205548c3b
anmol <anmolsoin1@gmail.com>
2022-01-10 11:59:20 +0530

    Some more questions

--- src/Assignment1/Product_Of_Array_Except_Self.java
+++ src/Assignment1/Product_Of_Array_Except_Self.java
@@ -1,4 +1,23 @@
 package Assignment1;
 
+import java.util.Scanner;
+
 public class Product_Of_Array_Except_Self {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int index = 0; index < arr.length; index++){
+            arr[index] = sc.nextInt();
+        }
+        for(int index_i = 0; index_i < arr.length; index_i++){
+            long product = 1;
+            for(int index_j = 0; index_j < arr.length; index_j++){
+                if(index_j != index_i){
+                    product *= arr[index_j];
+                }
+            }
+            System.out.print(product + " ");
+        }
+    }
 }
