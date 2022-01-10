039119e6af4f1304f2e3beb604d4cdec6203ade0
anmol <anmolsoin1@gmail.com>
2022-01-10 12:05:36 +0530

    Some more questions

---
+++ src/Assignment1/Squares_Of_A_Sorted_Array.java
@@ -0,0 +1,9 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+public class Squares_Of_A_Sorted_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
