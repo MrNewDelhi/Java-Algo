14f730d9f0fad5b10bf83b3855d921545f9c7c4f
anmol <anmolsoin1@gmail.com>
2022-01-24 21:38:45 +0530

    Some more questions

---
+++ src/Lec_13/ReverseWordsInASentence.java
@@ -0,0 +1,18 @@
+package Lec_13;
+
+//Input : The sky is blue
+//Output : blue is sky The
+public class ReverseWordsInASentence {
+    public static void main(String[] args) {
+        String str = "The sky is blue";
+        tokenization(str);
+
+    }
+    public static void tokenization(String str){
+        while(str.lastIndexOf(" ") != -1){
+            System.out.println(str.substring(str.lastIndexOf(" ")+1));
+            str = str.substring(0, str.lastIndexOf(" "));
+        }
+//        System.out.println(str+);
+    }
+}
