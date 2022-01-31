cc8ef2208db5873a9ead9cf0ca3480eb1dadef89
anmol <anmolsoin1@gmail.com>
2022-01-31 18:33:44 +0530

    Some more questions

--- src/Lec_14/AL_demo2.java
+++ src/Lec_14/AL_demo2.java
@@ -1,14 +1,45 @@
 package Lec_14;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
 
 public class AL_demo2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer I = sc.nextInt();
         ArrayList<Integer> AL = new ArrayList<>();
         AL.add(10);
         AL.add(20);
         AL.add(30);
         AL.add(40);
         System.out.println(AL);
+        System.out.println(AL.get(0));
+        //SIZE
+        System.out.println(AL.size());
+        System.out.println(AL.get(AL.size()-1));
+
+        AL.set(2,null);
+        System.out.println(AL);
+
+        //2-D ArrayList
+        ArrayList<ArrayList<Integer>> AL2 = new ArrayList<>();
+        AL2.add(new ArrayList<Integer>());
+        AL2.get(0).add(5);
+        System.out.println(AL2);
+        AL2.add(new ArrayList<Integer>());
+        AL2.get(1).add(9);
+        System.out.println(AL2);
+
+        //insert == add at index
+        AL.add(2,100);
+        System.out.println(AL);
+
+        //Remove
+        AL.remove(0);
+        System.out.println(AL);
+
+        int removedElement = AL.remove(0);
+        System.out.println(removedElement);
     }
 }
