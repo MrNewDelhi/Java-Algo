44b2cab0029cc82e4fefad292aab6ec2276605e3
anmol <anmolsoin1@gmail.com>
2022-01-31 22:24:34 +0530

    Some more questions

---
+++ src/Lec_14/IntersectionOfTwoArrays.java
@@ -0,0 +1,20 @@
+package Lec_14;
+
+import java.util.ArrayList;
+
+public class IntersectionOfTwoArrays {
+    public static void main(String[] args) {
+        int[] arr1 = new int[] {5,7,10,10,10,20,30,30,50,60,60,80};
+        int[] arr2 = new int[] {10,10,15,20,30,30,30,60,70,80,80,90};
+    }
+
+    public static ArrayList<Integer> Intersection(int[] arr1, int[] arr2) {
+        ArrayList<Integer> AL = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while(i < arr1.length && j < arr2.length){
+
+        }
+        return AL;
+    }
+}
