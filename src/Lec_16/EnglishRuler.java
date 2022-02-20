f7bb41bc48bdbc657cd25b53d5b8dd436f0f96b1
anmol <anmolsoin1@gmail.com>
2022-02-21 03:20:42 +0530

    Some more questions

---
+++ src/Lec_16/EnglishRuler.java
@@ -0,0 +1,12 @@
+package Lec_16;
+
+public class EnglishRuler {
+    public static void main(String[] args) {
+        int majorTick = 3;
+        int scale = 3;
+
+    }
+    public static void engRuler(int majorTick, int scale){
+
+    }
+}
